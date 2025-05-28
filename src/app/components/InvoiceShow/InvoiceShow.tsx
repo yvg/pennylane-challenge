40df865e3e2eb6e651284ad43bb948d40d0a7a8c
yvg <yves.vangoethem@tooltime.de>
2025-05-28 13:57:22 +0200

    use named exports

--- src/app/components/InvoiceShow/InvoiceShow.tsx
+++ src/app/components/InvoiceShow/InvoiceShow.tsx
@@ -4,7 +4,7 @@ import { useParams } from 'react-router'
 import { useApi } from 'api'
 import { Invoice } from 'types'
 
-const InvoiceShow = () => {
+export const InvoiceShow = () => {
   const { id } = useParams<{ id: string }>()
   const api = useApi()
   const [invoice, setInvoice] = useState<Invoice>()
