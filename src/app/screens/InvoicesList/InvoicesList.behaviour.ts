d6114cea3e581534cd42bd51a02e92063ec9307f
yvg <yves.vangoethem@tooltime.de>
2025-05-31 12:57:51 +0200

    Add creation of invoice for customer

--- src/app/screens/InvoicesList/InvoicesList.behaviour.ts
+++ src/app/screens/InvoicesList/InvoicesList.behaviour.ts
@@ -1,49 +1,98 @@
 import { useApi } from 'api'
 import { useCallback, useEffect, useState } from 'react'
-import { Invoice } from 'types'
+import { useNavigate } from 'react-router-dom'
+import { Customer, Invoice } from 'types'
 
-export const useInvoicesListBehaviour = () => {
-  const api = useApi()
-
-  const [invoicesList, setInvoicesList] = useState<Invoice[]>([])
-
-  const fetchInvoices = useCallback(async () => {
-    const { data } = await api.getInvoices()
-    setInvoicesList(data.invoices)
-  }, [api])
-
-  const deleteInvoice = useCallback(
-    async (id: number) => {
-      api
-        .deleteInvoice(id)
-        .then(fetchInvoices)
-        .catch((error) => {
-          // A no-op error handler
-          // Ideally, we would show a message to the user
-        })
-    },
-    [api, fetchInvoices]
-  )
-
-  const onClickDelete = useCallback(
-    (id: number) => (event: React.MouseEvent<HTMLButtonElement>) => {
-      event.preventDefault()
-      deleteInvoice(id)
-    },
-    [deleteInvoice]
-  )
-
-  useEffect(() => {
-    fetchInvoices()
-  }, [])
-
-  return {
-    handlers: {
-      onClickDelete,
-    },
-
-    states: {
-      invoicesList,
-    },
+type UseInvoicesListBehaviourReturnType = {
+  handlers: {
+    onClickDelete: (
+      id: number
+    ) => (event: React.MouseEvent<HTMLButtonElement>) => void
+    onClickAdd: () => void
+    onChangeCustomer: (customer: Invoice['customer'] | null) => void
+  }
+  states: {
+    invoicesList: Invoice[]
+    isCreateButtonDisabled: boolean
+    customerToAdd: Customer | null
   }
 }
+
+export const useInvoicesListBehaviour =
+  (): UseInvoicesListBehaviourReturnType => {
+    const api = useApi()
+    const navigate = useNavigate()
+    const [isCreateButtonDisabled, setIsCreateButtonDisabled] = useState(true)
+    const [customerToAdd, setCustomerToAdd] = useState<Customer | null>(null)
+
+    const [invoicesList, setInvoicesList] = useState<Invoice[]>([])
+
+    const fetchInvoices = useCallback(async () => {
+      const { data } = await api.getInvoices()
+      setInvoicesList(data.invoices)
+    }, [api])
+
+    const deleteInvoice = useCallback(
+      async (id: number) => {
+        api
+          .deleteInvoice(id)
+          .then(fetchInvoices)
+          .catch((error) => {
+            // A no-op error handler
+            // Ideally, we would show a message to the user
+          })
+      },
+      [api, fetchInvoices]
+    )
+
+    const onClickDelete = useCallback(
+      (id: number) => (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault()
+        deleteInvoice(id)
+      },
+      [deleteInvoice]
+    )
+
+    const onClickAdd = useCallback(() => {
+      if (customerToAdd) {
+        api
+          .postInvoices(null, {
+            invoice: {
+              customer_id: customerToAdd.id,
+            },
+          })
+          .then((response) => {
+            navigate(`/invoice/${response.data.id}`)
+          })
+      }
+      // Navigate to the add invoice page
+    }, [navigate, api, customerToAdd])
+
+    const onChangeCustomer = useCallback(
+      (customer: Invoice['customer'] | null) => {
+        if (customer) {
+          setCustomerToAdd(customer)
+          setIsCreateButtonDisabled(false)
+        }
+      },
+      [setCustomerToAdd, setIsCreateButtonDisabled]
+    )
+
+    useEffect(() => {
+      fetchInvoices()
+    }, [])
+
+    return {
+      handlers: {
+        onClickDelete,
+        onClickAdd,
+        onChangeCustomer,
+      },
+
+      states: {
+        invoicesList,
+        isCreateButtonDisabled,
+        customerToAdd,
+      },
+    }
+  }
--- src/app/screens/InvoicesList/InvoicesList.tsx
+++ src/app/screens/InvoicesList/InvoicesList.tsx
@@ -1,53 +1,66 @@
 import { Link } from 'react-router-dom'
 import { useInvoicesListBehaviour } from './InvoicesList.behaviour'
+import CustomerAutocomplete from 'app/shared/components/CustomerAutocomplete'
+
+import './InvoicesList.css'
 
 export const InvoicesList = (): React.ReactElement => {
 
   const {
-    states: { invoicesList },
-    handlers: { onClickDelete }
+    states: { invoicesList, isCreateButtonDisabled, customerToAdd },
+    handlers: { onClickDelete, onClickAdd, onChangeCustomer },
   } = useInvoicesListBehaviour()
 
   return (
-    <table className="table table-bordered table-striped">
-      <thead>
-        <tr>
-          {/* Ideally this should be in a locale file */}
-          <th>Id</th>
-          <th>Customer</th>
-          <th>Address</th>
-          <th>Customer ID</th>
-          <th>Total</th>
-          <th>Tax</th>
-          <th>Finalized</th>
-          <th>Paid</th>
-          <th>Date</th>
-          <th>Deadline</th>
-        </tr>
-      </thead>
-      <tbody>
-        {invoicesList.map((invoice) => (
-          <tr key={invoice.id}>
-            <td><Link to={`/invoice/${invoice.id}`}>{invoice.id}</Link></td>
-            <td>
-              {invoice.customer?.first_name} {invoice.customer?.last_name}
-            </td>
-            <td>
-              {invoice.customer?.address}, {invoice.customer?.zip_code}{' '}
-              {invoice.customer?.city}
-            </td>
-            <td>{invoice.customer_id}</td>
-            <td>{invoice.total}</td>
-            <td>{invoice.tax}</td>
-            <td>{invoice.finalized ? 'Yes' : 'No'}</td>
-            <td>{invoice.paid ? 'Yes' : 'No'}</td>
-            <td>{invoice.date}</td>
-            <td>{invoice.deadline}</td>
-            <td>{invoice.invoice_lines.length}</td>
-            <td><button onClick={onClickDelete(invoice.id)} disabled={invoice.finalized}>Delete</button></td>
+    <div>
+      <div className={'invoiceList-header'}>
+        <h1>Invoices</h1>
+        <div>
+          <CustomerAutocomplete onChange={onChangeCustomer} value={customerToAdd} />
+          <button disabled={isCreateButtonDisabled} onClick={onClickAdd}>Create Invoice</button>
+        </div>
+      </div>
+      {/* I'm not sure this should be considered tabular data…? */}
+      <table className="table table-bordered table-striped">
+        <thead>
+          <tr>
+            {/* Ideally this should be in a locale file */}
+            <th>Id</th>
+            <th>Customer</th>
+            <th>Address</th>
+            <th>Customer ID</th>
+            <th>Total</th>
+            <th>Tax</th>
+            <th>Finalized</th>
+            <th>Paid</th>
+            <th>Date</th>
+            <th>Deadline</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {invoicesList.map((invoice) => (
+            <tr key={invoice.id}>
+              <td><Link to={`/invoice/${invoice.id}`}>{invoice.id}</Link></td>
+              <td>
+                {invoice.customer?.first_name} {invoice.customer?.last_name}
+              </td>
+              <td>
+                {invoice.customer?.address}, {invoice.customer?.zip_code}{' '}
+                {invoice.customer?.city}
+              </td>
+              <td>{invoice.customer_id}</td>
+              <td>{invoice.total}</td>
+              <td>{invoice.tax}</td>
+              <td>{invoice.finalized ? 'Yes' : 'No'}</td>
+              <td>{invoice.paid ? 'Yes' : 'No'}</td>
+              <td>{invoice.date}</td>
+              <td>{invoice.deadline}</td>
+              <td>{invoice.invoice_lines.length}</td>
+              <td><button onClick={onClickDelete(invoice.id)} disabled={invoice.finalized}>Delete</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
